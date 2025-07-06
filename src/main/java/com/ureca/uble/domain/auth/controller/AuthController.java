c9d1cf3b9028bb20607b851edb641d39cd147e79
hayong39 <yia0524_hy@naver.com>
2025-07-06 21:12:04 +0900

    [UBLE-10] fix: 쿠키값 검증 추가

--- src/main/java/com/ureca/uble/domain/auth/controller/AuthController.java
+++ src/main/java/com/ureca/uble/domain/auth/controller/AuthController.java
@@ -13,6 +13,7 @@ import com.ureca.uble.domain.auth.dto.response.ReissueRes;
 import com.ureca.uble.domain.auth.exception.AuthErrorCode;
 import com.ureca.uble.domain.auth.service.AuthService;
 import com.ureca.uble.entity.User;
+import com.ureca.uble.global.exception.GlobalException;
 import com.ureca.uble.global.response.CommonResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -36,14 +37,20 @@ public class AuthController {
 
 	@PostMapping("/reissue")
 	@Operation(summary = "토큰 재발급", description = "Refresh Token을 통해 새로운 Access Token을 발급합니다.")
-	public CommonResponse<ReissueRes> reissue(@CookieValue("refreshToken") String refreshToken, HttpServletResponse response){
+	public CommonResponse<ReissueRes> reissue(@CookieValue(value = "refreshToken", required = false) String refreshToken, HttpServletResponse response){
+		if (refreshToken == null) {
+			throw new GlobalException(AuthErrorCode.REFRESH_TOKEN_NOT_FOUND);
+		}
 		authService.reissue(refreshToken, response);
 		return CommonResponse.success(new ReissueRes());
 	}
 
 	@PostMapping("/logout")
 	@Operation(summary = "로그아웃", description = "Refresh Token을 만료시키고 로그아웃합니다.")
-	public CommonResponse<LogoutRes> logout(@CookieValue("refreshToken") String refreshToken, HttpServletResponse response){
+	public CommonResponse<LogoutRes> logout(@CookieValue(value = "refreshToken", required = false) String refreshToken, HttpServletResponse response){
+		if (refreshToken == null) {
+			throw new GlobalException(AuthErrorCode.REFRESH_TOKEN_NOT_FOUND);
+		}
 		authService.logout(refreshToken, response);
 		return CommonResponse.success(new LogoutRes());
 	}
--- src/main/java/com/ureca/uble/domain/auth/exception/AuthErrorCode.java
+++ src/main/java/com/ureca/uble/domain/auth/exception/AuthErrorCode.java
@@ -11,9 +11,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ResultCode {
 	INVALID_TOKEN(HttpStatus.UNAUTHORIZED, 1000, "유효하지 않은 토큰입니다."),
-	UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, 1001, "인가되지 않은 접근입니다."),
-	REISSUE_SUCCESS(HttpStatus.OK, 1002, "토큰 재발급에 성공했습니다. "),
-	LOGOUT_SUCCESS(HttpStatus.OK, 1003, "로그아웃에 성공했습니다.");
+	REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, 1001, "Refresh token이 존재하지 않습니다."),
+	UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, 1002, "인가되지 않은 접근입니다.");
 
 	private final HttpStatus status;
 	private final int code;
