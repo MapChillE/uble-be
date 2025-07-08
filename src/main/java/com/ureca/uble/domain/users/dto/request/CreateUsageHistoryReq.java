30cf24f9e3c7d00fbba88a5ee1c3939bc33adc39
Yeoeun Yang <yyang78077@naver.com>
2025-07-08 17:33:57 +0900

    [UBLE-17] test: 이용 내역 등록 test 작성

--- src/main/java/com/ureca/uble/domain/users/dto/request/CreateUsageHistoryReq.java
+++ src/main/java/com/ureca/uble/domain/users/dto/request/CreateUsageHistoryReq.java
@@ -2,9 +2,11 @@ package com.ureca.uble.domain.users.dto.request;
 
 import com.ureca.uble.entity.enums.BenefitType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 @Schema(description = "이용 내역 생성 DTO")
 public class CreateUsageHistoryReq {
     @Schema(description = "사용 혜택 종류", example = "VIP")
--- src/main/java/com/ureca/uble/entity/enums/BenefitType.java
+++ src/main/java/com/ureca/uble/entity/enums/BenefitType.java
@@ -1,5 +1,8 @@
 package com.ureca.uble.entity.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum BenefitType {
     VIP,
     LOCAL,
