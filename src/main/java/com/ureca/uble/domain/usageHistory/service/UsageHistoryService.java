1a88ec80a274a2aaf1d63c584f020654e859d623
hayong39 <yia0524_hy@naver.com>
2025-07-08 11:14:20 +0900

    [UBLE-13] fix: UsageHistoryRepository 추가

--- src/main/java/com/ureca/uble/domain/usageHistory/service/UsageHistoryService.java
+++ src/main/java/com/ureca/uble/domain/usageHistory/service/UsageHistoryService.java
@@ -3,8 +3,7 @@ package com.ureca.uble.domain.usageHistory.service;
 import org.springframework.stereotype.Service;
 
 import com.ureca.uble.domain.usageHistory.dto.response.UsageHistoryRes;
-import com.ureca.uble.domain.usageHistory.repository.CustomUsageHistoryRepository;
-import com.ureca.uble.entity.UsageHistory;
+import com.ureca.uble.domain.usageHistory.repository.UsageHistoryRepository;
 import com.ureca.uble.global.dto.response.CursorPageRes;
 
 import lombok.RequiredArgsConstructor;
@@ -13,7 +12,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UsageHistoryService {
 
-	private final CustomUsageHistoryRepository usageRepository;
+	private final UsageHistoryRepository usageRepository;
 
 	public CursorPageRes<UsageHistoryRes> getUsageHistory(Long userId, Long lastHistoryId, int size) {
 		return usageRepository.findUsagesByUserId(userId, lastHistoryId, size);
