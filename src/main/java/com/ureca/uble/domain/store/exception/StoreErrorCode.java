3c68fc0d5a36eef75e02ae46bc45d5b049e73692
Yeoeun Yang <yyang78077@naver.com>
2025-07-09 14:33:40 +0900

    [UBLE-18] fix: input 범위 검증 로직 추가

--- src/main/java/com/ureca/uble/domain/store/exception/StoreErrorCode.java
+++ src/main/java/com/ureca/uble/domain/store/exception/StoreErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum StoreErrorCode implements ResultCode {
     STORE_NOT_FOUND(HttpStatus.NOT_FOUND, 5000, "매장 정보를 찾을 수 없습니다."),
+    OUT_OF_RANGE_INPUT(HttpStatus.BAD_REQUEST, 5001, "입력 값이 허용 범위를 벗어났습니다."),
     ;
 
     private final HttpStatus status;
