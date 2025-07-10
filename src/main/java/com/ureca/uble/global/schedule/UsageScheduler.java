c85836a6a07508cdd25b3e0319fa86e1b1580770
Yeoeun Yang <yyang78077@naver.com>
2025-07-10 22:58:13 +0900

    [UBLE-19] fix: 스케줄러 최종 수정

--- src/main/java/com/ureca/uble/global/schedule/UsageScheduler.java
+++ src/main/java/com/ureca/uble/global/schedule/UsageScheduler.java
@@ -19,8 +19,7 @@ public class UsageScheduler {
     private final RedisLockUtil redisLockUtil;
 
     // 하루에 한 번 (매일 자정)
-//    @Scheduled(cron = "0 0 0 * * *")
-    @Scheduled(cron = "0 0,6,12,18,24,30,36,42,48,54 * * * *")
+    @Scheduled(cron = "0 0 0 * * *")
     public void updateDaily() {
         redisLockUtil.executeWithLockWithRetry("normal-daily-lock", 1, 300, () -> {
             log.info("[NORMAL] DAILY 초기화 작업 시작: {}", LocalDateTime.now());
@@ -30,8 +29,7 @@ public class UsageScheduler {
     }
 
     // 일주에 한 번 (매주 월요일 자정)
-//    @Scheduled(cron = "0 0 0 * * 1")
-    @Scheduled(cron = "0 1,7,13,19,25,31,37,43,49,55 * * * *")
+    @Scheduled(cron = "0 0 0 * * 1")
     public void updateWeekly() {
         redisLockUtil.executeWithLockWithRetry("normal-weekly-lock", 1, 300, () -> {
             log.info("[NORMAL] WEEKLY 초기화 작업 시작: {}", LocalDateTime.now());
@@ -41,8 +39,7 @@ public class UsageScheduler {
     }
 
     // 한달에 한 번 (매월 1일 자정)
-//    @Scheduled(cron = "0 0 0 1 * *")
-    @Scheduled(cron = "0 2,8,14,20,26,32,38,44,50,56 * * * *")
+    @Scheduled(cron = "0 0 0 1 * *")
     public void updateMonthly() {
         redisLockUtil.executeWithLockWithRetry("normal-monthly-lock", 1, 300, () -> {
             log.info("[NORMAL] MONTHLY 초기화 작업 시작: {}", LocalDateTime.now());
@@ -52,8 +49,7 @@ public class UsageScheduler {
     }
 
     // 일년에 한 번 (매년 1월 1일 자정)
-//    @Scheduled(cron = "0 0 0 1 1 *")
-    @Scheduled(cron = "0 3,9,15,21,27,33,39,45,51,57 * * * *")
+    @Scheduled(cron = "0 0 0 1 1 *")
     public void updateYearly() {
         redisLockUtil.executeWithLockWithRetry("normal-yearly-lock", 1, 300, () -> {
             log.info("[NORMAL] YEARLY 초기화 작업 시작: {}", LocalDateTime.now());
@@ -63,8 +59,7 @@ public class UsageScheduler {
     }
 
     // 한달에 한 번 (매월 1일 자정)
-//    @Scheduled(cron = "0 0 0 1 * *")
-    @Scheduled(cron = "0 4,10,16,22,28,34,40,46,52,58 * * * *")
+    @Scheduled(cron = "0 0 0 1 * *")
     public void updateVip() {
         redisLockUtil.executeWithLockWithRetry("vip-monthly-lock", 1, 300, () -> {
             log.info("[VIP] MONTHLY 초기화 작업 시작: {}", LocalDateTime.now());
@@ -74,8 +69,7 @@ public class UsageScheduler {
     }
 
     // 하루에 한 번 (매일 자정)
-//    @Scheduled(cron = "0 0 0 * * *")
-    @Scheduled(cron = "0 5,11,17,23,29,35,41,47,53,59 * * * *")
+    @Scheduled(cron = "0 0 0 * * *")
     public void updateLocal() {
         redisLockUtil.executeWithLockWithRetry("local-daily-lock", 1, 300, () -> {
             log.info("[LOCAL] DAILY 초기화 작업 시작: {}", LocalDateTime.now());
