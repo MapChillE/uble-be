6e9f27d044b2abc9a8d12d2c00c94f813a22976b
hayong39 <yia0524_hy@naver.com>
2025-07-10 15:50:34 +0900

    [UBLE-23] fix: 이미 삭제된 사용자인 경우 추가

--- src/main/java/com/ureca/uble/domain/users/exception/UserErrorCode.java
+++ src/main/java/com/ureca/uble/domain/users/exception/UserErrorCode.java
@@ -11,7 +11,7 @@ public enum UserErrorCode implements ResultCode {
     // 사용자 2000번대
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, 2000, "사용자를 찾을 수 없습니다."),
     BENEFIT_NOT_AVAILABLE(HttpStatus.CONFLICT, 2001, "혜택을 사용할 수 없습니다."),
-    ;
+	USER_ALREADY_DELETED(HttpStatus.BAD_REQUEST, 2002, "이미 삭제된 사용자입니다.");
 
     private final HttpStatus status;
     private final int code;
