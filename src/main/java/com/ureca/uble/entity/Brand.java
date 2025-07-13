3b277cd09c1402dac092eed30d69b5afaa759cbe
hayong39 <yia0524_hy@naver.com>
2025-07-13 20:14:48 +0900

    [UBLE-30] style: import문 수정

--- src/main/java/com/ureca/uble/entity/Brand.java
+++ src/main/java/com/ureca/uble/entity/Brand.java
@@ -9,16 +9,7 @@ import java.util.List;
 import com.ureca.uble.entity.enums.Rank;
 import com.ureca.uble.entity.enums.RankType;
 import com.ureca.uble.entity.enums.Season;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
