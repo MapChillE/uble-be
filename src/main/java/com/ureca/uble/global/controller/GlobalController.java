5b017cc86f210e4b602ccf5c096f514eb6208f61
hayong39 <yia0524_hy@naver.com>
2025-07-15 16:19:20 +0900

    fix: 오타 수정 및 클래스 네임 수정

--- src/main/java/com/ureca/uble/global/controller/GlobalController.java
+++ src/main/java/com/ureca/uble/global/controller/GlobalController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ureca.uble.global.response.CommonResponse;
 
 @RestController
-public class LoadBalanceContorller {
+public class GlobalController {
 	@GetMapping("/health")
 	public CommonResponse<String> healthCheck() {
 		return CommonResponse.success("OK");
