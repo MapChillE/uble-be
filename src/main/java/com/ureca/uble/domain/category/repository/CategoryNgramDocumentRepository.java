2ac00a6c7b9dfbdcf6e4990ae41b3d83fe4f0283
Yeoeun Yang <yyang78077@naver.com>
2025-07-15 20:16:26 +0900

    [UBLE-32] rename: 파일 위치 수정

--- src/main/java/com/ureca/uble/domain/category/repository/CategoryNgramDocumentRepository.java
+++ src/main/java/com/ureca/uble/domain/category/repository/CategoryNgramDocumentRepository.java
@@ -1,4 +1,4 @@
-package com.ureca.uble.domain.brand.repository;
+package com.ureca.uble.domain.category.repository;
 
 import com.ureca.uble.entity.document.CategoryNgramDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
