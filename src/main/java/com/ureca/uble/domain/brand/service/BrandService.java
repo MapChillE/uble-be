090fbbc15856d3ffba7cdd78ed7162971533e769
Yeoeun Yang <yyang78077@naver.com>
2025-07-19 00:05:07 +0900

    feat: 제휴처 상세 조회 로그 추가 구현

--- src/main/java/com/ureca/uble/domain/brand/service/BrandService.java
+++ src/main/java/com/ureca/uble/domain/brand/service/BrandService.java
@@ -1,39 +1,38 @@
 package com.ureca.uble.domain.brand.service;
 
-import java.util.ArrayList;
+import com.ureca.uble.domain.bookmark.repository.BookmarkRepository;
+import com.ureca.uble.domain.brand.dto.response.BenefitDetailRes;
+import com.ureca.uble.domain.brand.dto.response.BrandDetailRes;
+import com.ureca.uble.domain.brand.dto.response.BrandListRes;
+import com.ureca.uble.domain.brand.dto.response.SearchBrandListRes;
+import com.ureca.uble.domain.brand.exception.BrandErrorCode;
+import com.ureca.uble.domain.brand.repository.BrandClickLogDocumentRepository;
+import com.ureca.uble.domain.brand.repository.BrandNoriDocumentRepository;
+import com.ureca.uble.domain.brand.repository.BrandRepository;
+import com.ureca.uble.domain.common.dto.response.CursorPageRes;
+import com.ureca.uble.domain.users.repository.UserRepository;
+import com.ureca.uble.entity.Bookmark;
+import com.ureca.uble.entity.Brand;
+import com.ureca.uble.entity.User;
+import com.ureca.uble.entity.document.BrandClickLogDocument;
+import com.ureca.uble.entity.document.BrandNoriDocument;
+import com.ureca.uble.entity.enums.BenefitType;
+import com.ureca.uble.entity.enums.Rank;
+import com.ureca.uble.entity.enums.RankType;
+import com.ureca.uble.entity.enums.Season;
+import com.ureca.uble.global.exception.GlobalException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import co.elastic.clients.elasticsearch._types.FieldValue;
-import co.elastic.clients.elasticsearch._types.query_dsl.*;
-import com.ureca.uble.domain.brand.dto.response.*;
-import com.ureca.uble.domain.brand.repository.BrandNoriDocumentRepository;
-import com.ureca.uble.entity.document.BrandNoriDocument;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.elasticsearch.client.elc.NativeQuery;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.ureca.uble.domain.bookmark.repository.BookmarkRepository;
-import com.ureca.uble.domain.brand.exception.BrandErrorCode;
-import com.ureca.uble.domain.brand.repository.BrandRepository;
-import com.ureca.uble.entity.Bookmark;
-import com.ureca.uble.entity.Brand;
-import com.ureca.uble.entity.enums.BenefitType;
-import com.ureca.uble.entity.enums.Rank;
-import com.ureca.uble.entity.enums.RankType;
-import com.ureca.uble.entity.enums.Season;
-import com.ureca.uble.global.exception.GlobalException;
-import com.ureca.uble.domain.common.dto.response.CursorPageRes;
-
-import lombok.RequiredArgsConstructor;
-
-import static com.ureca.uble.domain.brand.exception.BrandErrorCode.ELASTIC_INTERNAL_ERROR;
+import static com.ureca.uble.domain.users.exception.UserErrorCode.USER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -42,12 +41,15 @@ public class BrandService {
 	private final BrandRepository brandRepository;
 	private final BookmarkRepository bookmarkRepository;
 	private final BrandNoriDocumentRepository brandNoriDocumentRepository;
+	private final BrandClickLogDocumentRepository brandClickLogDocumentRepository;
+	private final UserRepository userRepository;
 
 	/**
 	 * 제휴처 상세 조회
 	 */
 	@Transactional(readOnly = true)
 	public BrandDetailRes getBrandDetail(Long userId, Long brandId) {
+		User user = findUser(userId);
 		Brand brand = brandRepository.findWithBenefitsById(brandId)
 			.orElseThrow(() -> new GlobalException(BrandErrorCode.BRAND_NOT_FOUND));
 
@@ -63,6 +65,9 @@ public class BrandService {
 			.toList();
 
 		boolean isVIPcock = brand.isVIPcock();
+
+		// 로그 저장
+		brandClickLogDocumentRepository.save(BrandClickLogDocument.of(user, brand));
 
 		return BrandDetailRes.of(brand, isBookmarked, bookmarkId, isVIPcock, benefits);
 	}
@@ -142,5 +147,9 @@ public class BrandService {
 			.toList();
 
 		return SearchBrandListRes.of(brandList, totalCnt, totalPage);
+	}
+
+	private User findUser(Long userId) {
+		return userRepository.findById(userId).orElseThrow(() -> new GlobalException(USER_NOT_FOUND));
 	}
 }
