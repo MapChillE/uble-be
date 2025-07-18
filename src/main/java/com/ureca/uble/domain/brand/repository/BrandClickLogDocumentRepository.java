8dc717a2319afa88b644200236b72f4c12443400
Yeoeun Yang <yyang78077@naver.com>
2025-07-19 00:05:17 +0900

    feat: 제휴처 상세 조회 로그 추가 구현

---
+++ src/main/java/com/ureca/uble/domain/brand/repository/BrandClickLogDocumentRepository.java
@@ -0,0 +1,7 @@
+package com.ureca.uble.domain.brand.repository;
+
+import com.ureca.uble.entity.document.BrandClickLogDocument;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface BrandClickLogDocumentRepository extends ElasticsearchRepository<BrandClickLogDocument, String> {
+}
