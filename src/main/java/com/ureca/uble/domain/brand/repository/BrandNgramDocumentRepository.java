91f49ec366444235dc415276112af5d7fea0e566
Yeoeun Yang <yyang78077@naver.com>
2025-07-20 19:05:14 +0900

    rename: index 관련 파일 이름 변경

--- src/main/java/com/ureca/uble/domain/brand/repository/BrandNgramDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ureca.uble.domain.brand.repository;
-
-import com.ureca.uble.entity.document.BrandNgramDocument;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface BrandNgramDocumentRepository extends ElasticsearchRepository<BrandNgramDocument, String> {
-}
--- src/main/java/com/ureca/uble/domain/category/repository/CategoryNgramDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ureca.uble.domain.category.repository;
-
-import com.ureca.uble.entity.document.CategoryNgramDocument;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface CategoryNgramDocumentRepository extends ElasticsearchRepository<CategoryNgramDocument, String> {
-}
