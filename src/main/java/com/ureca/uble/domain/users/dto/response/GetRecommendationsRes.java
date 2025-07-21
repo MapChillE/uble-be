fe3a19117c57255373abf095bac2e9d6b7adf0dd
hayong39 <yia0524_hy@naver.com>
2025-07-21 11:21:43 +0900

    [UBLE-52] feat: 추천 정보 반환 기능 DTO 구현

---
+++ src/main/java/com/ureca/uble/domain/users/dto/response/GetRecommendationsRes.java
@@ -0,0 +1,35 @@
+package com.ureca.uble.domain.users.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(description = "추천 정보 반환 DTO")
+public class GetRecommendationsRes {
+
+    @Schema(description = "매장 id", example = "1")
+    private Long storeId;
+
+    @Schema(description = "매장 이름", example = "CGV 강남")
+    private String storeName;
+
+    @Schema(description = "카테고리", example = "문화/여가")
+    private String category;
+
+    @Schema(description = "제휴처 설명", example = "커피가 맛있는 집")
+    private String description;
+
+    @Schema(description = "VIP콕 대상 여부", example = "false")
+    @JsonProperty("isVIPcock")
+    private boolean vipcock;
+
+    @Schema(description = "최소 이용 등급", example = "NORMAL")
+    private String minRank;
+
+    @Schema(description = "제휴처 대표 이미지 url", example = "https://image.com")
+    private String imgUrl;
+}
---
+++ src/main/java/com/ureca/uble/domain/users/dto/response/GetRecommmendationListRes.java
@@ -0,0 +1,15 @@
+package com.ureca.uble.domain.users.dto.response;
+
+import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Schema(description = "추천 리스트 반환 DTO")
+public class GetRecommmendationListRes {
+    @Schema(description = "추천 리스트", example = "추천 리스트")
+    private List<GetRecommendationsRes> recommendationsList;
+}
