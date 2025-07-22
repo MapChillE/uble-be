2b12dbf798ef6ba5054d534e43dd374da68c7719
Yeoeun Yang <yyang78077@naver.com>
2025-07-23 00:35:24 +0900

    [UBLE-65] fix: 스케줄러 분산락 제거

--- src/main/java/com/ureca/uble/global/schedule/UsageScheduler.java
+++ src/main/java/com/ureca/uble/global/schedule/UsageScheduler.java
@@ -2,7 +2,6 @@ package com.ureca.uble.global.schedule;
 
 import com.ureca.uble.entity.enums.Period;
 import com.ureca.uble.global.schedule.batch.UsageBatchRunner;
-import com.ureca.uble.global.schedule.util.RedisLockUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,65 +15,46 @@ import java.time.LocalDateTime;
 public class UsageScheduler {
 
     private final UsageBatchRunner batchRunner;
-    private final RedisLockUtil redisLockUtil;
 
     // 하루에 한 번 (매일 자정)
     @Scheduled(cron = "0 0 0 * * *")
     public void updateDaily() {
-        redisLockUtil.executeWithLockWithRetry("normal-daily-lock", 1, 300, () -> {
-            log.info("[NORMAL] DAILY 초기화 작업 시작: {}", LocalDateTime.now());
-            batchRunner.runNormalBatch(Period.DAILY);
-            return null;
-        });
+        log.info("[NORMAL] DAILY 초기화 작업 시작: {}", LocalDateTime.now());
+        batchRunner.runNormalBatch(Period.DAILY);
     }
 
     // 일주에 한 번 (매주 월요일 자정)
     @Scheduled(cron = "0 0 0 * * 1")
     public void updateWeekly() {
-        redisLockUtil.executeWithLockWithRetry("normal-weekly-lock", 1, 300, () -> {
-            log.info("[NORMAL] WEEKLY 초기화 작업 시작: {}", LocalDateTime.now());
-            batchRunner.runNormalBatch(Period.WEEKLY);
-            return null;
-        });
+        log.info("[NORMAL] WEEKLY 초기화 작업 시작: {}", LocalDateTime.now());
+        batchRunner.runNormalBatch(Period.WEEKLY);
     }
 
     // 한달에 한 번 (매월 1일 자정)
     @Scheduled(cron = "0 0 0 1 * *")
     public void updateMonthly() {
-        redisLockUtil.executeWithLockWithRetry("normal-monthly-lock", 1, 300, () -> {
-            log.info("[NORMAL] MONTHLY 초기화 작업 시작: {}", LocalDateTime.now());
-            batchRunner.runNormalBatch(Period.MONTHLY);
-            return null;
-        });
+        log.info("[NORMAL] MONTHLY 초기화 작업 시작: {}", LocalDateTime.now());
+        batchRunner.runNormalBatch(Period.MONTHLY);
     }
 
     // 일년에 한 번 (매년 1월 1일 자정)
     @Scheduled(cron = "0 0 0 1 1 *")
     public void updateYearly() {
-        redisLockUtil.executeWithLockWithRetry("normal-yearly-lock", 1, 300, () -> {
-            log.info("[NORMAL] YEARLY 초기화 작업 시작: {}", LocalDateTime.now());
-            batchRunner.runNormalBatch(Period.YEARLY);
-            return null;
-        });
+        log.info("[NORMAL] YEARLY 초기화 작업 시작: {}", LocalDateTime.now());
+        batchRunner.runNormalBatch(Period.YEARLY);
     }
 
     // 한달에 한 번 (매월 1일 자정)
     @Scheduled(cron = "0 0 0 1 * *")
     public void updateVip() {
-        redisLockUtil.executeWithLockWithRetry("vip-monthly-lock", 1, 300, () -> {
-            log.info("[VIP] MONTHLY 초기화 작업 시작: {}", LocalDateTime.now());
-            batchRunner.runVipBatch();
-            return null;
-        });
+        log.info("[VIP] MONTHLY 초기화 작업 시작: {}", LocalDateTime.now());
+        batchRunner.runVipBatch();
     }
 
     // 하루에 한 번 (매일 자정)
     @Scheduled(cron = "0 0 0 * * *")
     public void updateLocal() {
-        redisLockUtil.executeWithLockWithRetry("local-daily-lock", 1, 300, () -> {
-            log.info("[LOCAL] DAILY 초기화 작업 시작: {}", LocalDateTime.now());
-            batchRunner.runLocalBatch();
-            return null;
-        });
+        log.info("[LOCAL] DAILY 초기화 작업 시작: {}", LocalDateTime.now());
+        batchRunner.runLocalBatch();
     }
 }
