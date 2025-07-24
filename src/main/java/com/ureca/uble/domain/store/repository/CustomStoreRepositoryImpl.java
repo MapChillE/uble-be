73a061154d9945173619d3f0269619efb5c32b2c
Eric-HAN-01 <hhw3422@gmail.com>
2025-07-25 02:10:38 +0900

    [UBLE-18] feat: 사각형 범위 내 매장 조회 기능 추가

--- src/main/java/com/ureca/uble/domain/store/repository/CustomStoreRepositoryImpl.java
+++ src/main/java/com/ureca/uble/domain/store/repository/CustomStoreRepositoryImpl.java
@@ -8,7 +8,6 @@ import com.ureca.uble.entity.enums.BenefitType;
 import com.ureca.uble.entity.enums.RankType;
 import com.ureca.uble.entity.enums.Season;
 import lombok.RequiredArgsConstructor;
-import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -27,36 +26,31 @@ public class CustomStoreRepositoryImpl implements CustomStoreRepository {
      * 근처 매장 정보 조회
      */
     @Override
-    public List<Store> findStoresByFiltering(Point curPoint, int distance, Long categoryId, Long brandId, Season season, BenefitType type) {
+    public List<Store> findStoresInBox(double swLng, double swLat, double neLng, double neLat,
+                                       Long categoryId, Long brandId, Season season, BenefitType type) {
         return jpaQueryFactory
-            .select(store)
-            .from(store)
-            .innerJoin(store.brand, brand).fetchJoin()
-            .innerJoin(brand.category, category).fetchJoin()
-            .where(
-                withinRadius(curPoint, distance),
-                categoryIdEq(categoryId),
-                brandIdEq(brandId),
-                seasonEq(season),
-                typeEq(type)
-            )
-            .fetch();
+                .select(store).from(store)
+                .innerJoin(store.brand, brand).fetchJoin()
+                .innerJoin(brand.category, category).fetchJoin()
+                .where(
+                        Expressions.booleanTemplate(
+                                "function('ST_Intersects', {0}, function('ST_MakeEnvelope', {1}, {2}, {3}, {4}, 4326)) = TRUE",
+                                store.location,
+                                swLng, swLat, neLng, neLat
+                        ),
+                        categoryIdEq(categoryId), brandIdEq(brandId), seasonEq(season), typeEq(type)
+                )
+                .fetch();
     }
 
     public BooleanExpression getCondition(BenefitType type) {
         return switch (type) {
-            case VIP -> brand.rankType.eq(RankType.VIP)
+              case VIP -> brand.rankType.eq(RankType.VIP)
                 .or(brand.rankType.eq(RankType.VIP_NORMAL));
             case NORMAL -> brand.rankType.eq(RankType.NORMAL)
                 .or(brand.rankType.eq(RankType.VIP_NORMAL));
             case LOCAL -> brand.isLocal.isTrue();
         };
-    }
-
-    private BooleanExpression withinRadius(Point curPoint, int distance) {
-        return Expressions.booleanTemplate(
-            "function('ST_DWithin', {0}, {1}, {2})", store.location, curPoint, distance
-        ).isTrue();
     }
 
     private BooleanExpression categoryIdEq(Long categoryId) {
