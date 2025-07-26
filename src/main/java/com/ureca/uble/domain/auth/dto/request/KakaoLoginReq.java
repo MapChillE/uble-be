cd268bb54df9749bd3d178f94e5c1fb6f2c79852
hayong39 <yia0524_hy@naver.com>
2025-07-26 15:59:40 +0900

    [UBLE-98] fix: 카카오 인가코드 제약 제거

--- src/main/java/com/ureca/uble/domain/auth/dto/request/KakaoLoginReq.java
+++ src/main/java/com/ureca/uble/domain/auth/dto/request/KakaoLoginReq.java
@@ -12,7 +12,6 @@ public class KakaoLoginReq {
 
 	@Schema(description = "카카오 인가 코드", example="abc123xyz")
 	@NotBlank(message = "인가 코드는 필수입니다.")
-	@Size(min = 10, max = 100, message = "인가 코드는 10자 이상 100자 이하로 입력해주세요.")
 	private String code;
 
 }
