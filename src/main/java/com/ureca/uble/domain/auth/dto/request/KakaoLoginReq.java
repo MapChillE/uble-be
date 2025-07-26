880937f71d75d5ace8582d2f2b77dddfd7a03a34
hayong39 <yia0524_hy@naver.com>
2025-07-26 15:49:24 +0900

    [UBLE-98] fix: Auth 도메인 validation 추가

--- src/main/java/com/ureca/uble/domain/auth/dto/request/KakaoLoginReq.java
+++ src/main/java/com/ureca/uble/domain/auth/dto/request/KakaoLoginReq.java
@@ -2,6 +2,7 @@ package com.ureca.uble.domain.auth.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
@@ -11,6 +12,7 @@ public class KakaoLoginReq {
 
 	@Schema(description = "카카오 인가 코드", example="abc123xyz")
 	@NotBlank(message = "인가 코드는 필수입니다.")
+	@Size(min = 10, max = 100, message = "인가 코드는 10자 이상 100자 이하로 입력해주세요.")
 	private String code;
 
 }
