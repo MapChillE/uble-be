b4aad34f2e0137316f881802ce95dbb27a3364d6
Yeoeun Yang <yyang78077@naver.com>
2025-07-27 19:20:15 +0900

    [UBLE-103] fix: store DTO 수정

--- src/main/java/com/ureca/uble/domain/store/dto/response/GetGlobalSuggestionRes.java
+++ src/main/java/com/ureca/uble/domain/store/dto/response/GetGlobalSuggestionRes.java
@@ -19,18 +19,31 @@ public class GetGlobalSuggestionRes {
     @Schema(description = "카테고리", example = "카페")
     private String category;
 
+    @Schema(description = "매장 id", example = "12")
+    private Long storeId;
+
+    @Schema(description = "위도", example = "37.123")
+    private Double latitude;
+
+    @Schema(description = "경도", example = "127.123")
+    private Double longitude;
+
     @Schema(description = "주소", example = "서울시 강남구 123")
     private String address;
 
     @Schema(description = "자동완성 타입", example = "BRAND")
     private SuggestionType type;
 
-    public static GetGlobalSuggestionRes of(String suggestion, String category, String address, SuggestionType type) {
+
+    public static GetGlobalSuggestionRes of(String suggestion, String category, String address, SuggestionType type, Long storeId, Double latitude, Double longitude) {
         return GetGlobalSuggestionRes.builder()
             .suggestion(suggestion)
             .category(category)
             .address(address)
             .type(type)
+            .storeId(storeId)
+            .latitude(latitude)
+            .longitude(longitude)
             .build();
     }
 }
